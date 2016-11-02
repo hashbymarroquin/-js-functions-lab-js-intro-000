336a5260eab5e987c8ea535c9f192e53adc1035d
hashby <hashbym37741@students.laalliance.org>
2016-11-02 15:31:24 +0000

    First saved version of my nitrous code.

---
+++ index.js
@@ -0,0 +1,13 @@
+// write your code below!
+var happyHolidays = function (happyHolidays){
+  return 'Happy holidays!';
+};
+var happyHolidaysTo = function (happyHolidaysTo){
+  return 'Happy holidays, you!';
+};
+var happyHolidayTo = function (happyHolidayTo){
+  return 'Happy Independence Day, you!';
+};
+var holidayCountdown = function (holidayCountdown){
+  return 'It\'s 20 days until Mother\'s Day!';
+};
